4dd2953204fa3ee03a4e95ece0ebc59c6ace9cd7
Tomas Sedovic <tomas@sedovic.cz>
2013-11-22 20:12:02 +0100

    Use scm_with_guile
    It's supposed to be more portable according to the manual.
    
    Signed-off-by: Tomas Sedovic <tomas@sedovic.cz>

--- main.c
+++ main.c
@@ -7,13 +7,18 @@ static SCM has_entity(SCM entity_id) {
 	return scm_from_bool(id > 0 && id < 10);
 }
 
-int main (int argc, char **argv) {
-	// TODO: scm_with_guile is preferable but doesn't work properly, see below.
-	scm_init_guile();
+static void* guile_main(void* data) {
 	scm_c_define_gsubr("has-entity", 1, 0, 0, &has_entity);
 	scm_c_primitive_load("main.scm");
-	// TODO: gauses a stack overflow when used with scm_with_guile instead of scm_init_guile
 	scm_call_0(scm_variable_ref(scm_c_lookup("foo")));
 	scm_call_0(scm_variable_ref(scm_c_lookup("bar")));
+	return NULL;
+}
+
+
+#define MAX_FILE_SIZE 1000000
+
+int main (int argc, char **argv) {
+	scm_with_guile(&guile_main, NULL);
 	return 0;
 }
