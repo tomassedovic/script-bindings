107e00d7279d5245d50a2cf7ac4f277259275b96
Tomas Sedovic <tomas@sedovic.cz>
2013-11-23 00:44:20 +0100

    Make the Lua stuff feature-compatible with Guile
    Both the C program and the Lua script file now demonstrate the same capabilities
    as the Guile code.
    
    Signed-off-by: Tomas Sedovic <tomas@sedovic.cz>

--- lua.c
+++ lua.c
@@ -4,9 +4,40 @@
 #include <lua.h>
 #include <lauxlib.h>
 
-int main(int argc, char **argv) {
+static int has_entity(lua_State *L) {
+	int arg_count = lua_gettop(L);
+	if(arg_count != 1) {
+		lua_pushstring(L, "`has_entity` expects a single argument");
+		lua_error(L);
+	}
+	// read the first argument from the stack
+	int id = luaL_checkint(L, 1);
+	// return a single boolean value
+	lua_pushboolean(L, id > 0 && id < 10); return 1;
+}
+
+static int factorial(lua_State *L) {
+	int arg_count = lua_gettop(L);
+	if(arg_count != 1) {
+		lua_pushstring(L, "`has_entity` expects a single argument");
+		lua_error(L);
+	}
+	int n = luaL_checkint(L, 1);
+	double result = 1.0;
+	int i = 0;
+	for(i = 1; i <= n; i++) {
+		result *= i;
+	}
+	lua_pushnumber(L, result); return 1;
+}
+
+void run_lua_script(char* script_name) {
 	lua_State *L = luaL_newstate();
 	luaL_openlibs(L);
+	// Define a C function in Lua
+	lua_pushcfunction(L, &has_entity); lua_setglobal(L, "has_entity");
+	lua_pushcfunction(L, &factorial); lua_setglobal(L, "factorial");
+
 	int status = luaL_loadfile(L, "main.lua");
 	if(status) {
 		fprintf(stderr, "Couldn't load file: %s\n", lua_tostring(L, -1));
@@ -20,5 +51,14 @@ int main(int argc, char **argv) {
 	lua_getglobal(L, "main");
 	lua_call(L, 0, 0);
 	lua_close(L);
+}
+
+int main(int argc, char **argv) {
+	if(argc == 2) {
+		run_lua_script(argv[1]);
+	} else {
+		printf("You must enter the path to the script.");
+		return 1;
+	}
 	return 0;
 }
--- main.lua
+++ main.lua
@@ -2,15 +2,15 @@ function foo()
    local ids = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
    result = {}
    for e in ipairs(ids) do
-	  -- TODO: call has_entity from C code here:
-	  table.insert(result, e % 2 == 0)
+	  table.insert(result, has_entity(e))
    end
    return result
 end
 
 function main()
-   print("Called the `main` main function")
-   print(table.unpack(foo()))
+   print("Called the `main` main function.")
+   print("Factorial:", factorial(10))
+   print("Entity existence map:", table.unpack(foo()))
 end
 
 print("Loaded main.lua!")
